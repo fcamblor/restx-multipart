17655d1cf3ce66f21ac175f6a5b0d416cdaac73a
fcamblor <frederic.camblor@4sh.fr>
2015-10-20 15:48:40 +0200

    Getting rid of servlet 3+ dependency and using commons-fileupload instead to handle multipart form data through dedicated injectable PartsReader

--- src/main/java/restx/multipart/module/FormDataEntityRequestBodyReader.java
+++ src/main/java/restx/multipart/module/FormDataEntityRequestBodyReader.java
@@ -1,28 +1,18 @@
 package restx.multipart.module;
 
-import com.google.common.io.Files;
-import org.eclipse.jetty.util.MultiPartInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import restx.RestxContext;
 import restx.RestxRequest;
-import restx.WebException;
-import restx.common.TypeReference;
 import restx.entity.EntityRequestBodyReader;
-import restx.http.HttpStatus;
+import restx.multipart.PartsReader;
 
-import javax.servlet.MultipartConfigElement;
-import javax.servlet.ServletException;
-import javax.servlet.http.Part;
-import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.Collection;
-import java.util.List;
 
-public class FormDataEntityRequestBodyReader implements EntityRequestBodyReader<Collection<Part>> {
+public class FormDataEntityRequestBodyReader implements EntityRequestBodyReader<PartsReader> {
 
-    private static final Logger logger = LoggerFactory.getLogger(FormDataEntityRequestBodyReader.class);
+    private static final Logger LOG = LoggerFactory.getLogger(FormDataEntityRequestBodyReader.class);
 
     @Override
     public Type getType() {
@@ -32,17 +22,7 @@ public class FormDataEntityRequestBodyReader implements EntityRequestBodyReader<
     //TODO Declare Multipart annotation to wrap @Consumes("multipart/form-data") and @MultipartConfig
     // to create MultipartConfigElement
     @Override
-    public Collection<Part> readBody(RestxRequest req, RestxContext ctx) throws IOException {
-        File tempDir = Files.createTempDir();
-        MultiPartInputStream multiPartInputStream =
-                new MultiPartInputStream(req.getContentStream(), req.getContentType(),
-                                         new MultipartConfigElement(tempDir.getAbsolutePath()),
-                                         tempDir);
-        try {
-            return multiPartInputStream.getParts();
-        } catch (ServletException e) {
-            logger.error("Can't parse request", e);
-            throw new WebException(HttpStatus.BAD_REQUEST, String.format("Can't parse request\n%s", e));
-        }
+    public PartsReader readBody(RestxRequest req, RestxContext ctx) throws IOException {
+        return new PartsReader(req);
     }
 }
